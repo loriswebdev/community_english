d338cec37ada3e8dc2a00f7634358226d2e25820
Robert <Robmel@Roberts-MacBook-Pro.local>
2024-08-14 10:00:48 -0600

    first commit

--- src/api/auth/getuser.ts
+++ src/api/auth/getuser.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { signIn } from '@/'
+
  
 export default async function handler(
   req: NextApiRequest,
@@ -7,7 +7,7 @@ export default async function handler(
 ) {
   try {
     const { email, id } = req.body
-    await signIn('credentials', { email, id })
+    
  
     res.status(200).json({ success: true })
   } catch (error: any) {
